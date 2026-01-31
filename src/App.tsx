4483cd946db5ac4525d4e57ca80c784c55545922
Kacper <kapi98981@gmail.com>
2026-01-31 13:51:51 +0100

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-neutral text-neutral-content">
+      <h1 className="text-4xl mb-4">Witaj w DaisyUI!</h1>
+      <button className="btn btn-primary">Przycisk DaisyUI</button>
+    </div>
+  );
+}
+
+export default App;
