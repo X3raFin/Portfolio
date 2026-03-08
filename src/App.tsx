c3d92b8076be235979598627f4a3caf824292264
Kacper <kapi98981@gmail.com>
2026-03-08 14:42:38 +0100

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SiteContent from "./Components/SiteContent";
 
 function App() {
   return (
-    <div className="h-full md:h-screen bg-base-100 md:p-2 flex">
+    <div className="h-full md:h-screen bg-base-100 md:p-2 flex flex-wrap">
       <div className="flex-1 flex bg-base-300 rounded-xl md:p-5 flex-col md:flex-row">
         <Navbar />
         <SiteContent />
